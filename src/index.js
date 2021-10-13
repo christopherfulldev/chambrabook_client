ef80a9a385fd7169a687aac2d0df7d896dd5de16
christopherfulldev <highlander.all@gmail.com>
2021-10-13 13:32:34 -0300

    Restructure

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import {BrowserRouter as Router} from "react-router-dom";
+import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
+  <Router>
     <App />
+  </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
